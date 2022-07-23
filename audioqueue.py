b4fdb81682c270cbed804337fbeb8cf04c9d5200
Will Hall <will@44ld.co.uk>
2022-07-23 21:56:29 +0100

    audiotest to async

--- audioqueue.py
+++ audioqueue.py
@@ -12,11 +12,13 @@
 # HISTORY:
 # Date      	By	Comments
 # ----------	---	---------------------------------------------------------
+# 2022-07-23	WH	Converted to async function
 # 2022-07-23	WH	System can successfully record audio samples to and from a queue
 #---------------------------------------------------------------------#
 import sys
 import queue
 import threading
+import asyncio
 
 import sounddevice as sd
 
@@ -25,14 +27,16 @@ BUFFER_SIZE = 80
 BLOCK_SIZE = 1024
 SAMPLE_RATE = 44100
 CHANNELS = 1
+DEVICE = "default"
 
 q = queue.Queue(maxsize=BUFFER_SIZE)
 
-def record_buffer():
+async def record_buffer():
     """
     Fill up a buffer queue with recorded sound
     """
-    event = threading.Event()
+    loop = asyncio.get_event_loop()
+    event = asyncio.Event()
     blx_buffered = 0
 
     def callback(indata, frame_count, time_info, status):
@@ -40,19 +44,22 @@ def record_buffer():
         
         # Once the buffer is full, stop
         if blx_buffered == BUFFER_SIZE:
-            event.set()
+            loop.call_soon_threadsafe(event.set)
             raise sd.CallbackStop
         
         q.put_nowait(bytes(indata))
         print("Samples Recorded: ", q.qsize())
         blx_buffered += 1
 
-    stream = sd.RawInputStream(samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE, callback=callback, dtype="float32", device="default",
+    stream = sd.RawInputStream(samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE, callback=callback, dtype="float32", device=DEVICE,
                             channels=CHANNELS)
     with stream:
-        event.wait()  # Wait until recording is finished
+        await event.wait()  # Wait until recording is finished
 
-def play_buffer():
+async def play_buffer():
+    event = asyncio.Event()
+    loop = asyncio.get_event_loop()
+
     def callback(outdata, frames, time, status):
         assert frames == BLOCK_SIZE
         if status.output_underflow:
@@ -70,20 +77,29 @@ def play_buffer():
         if len(data) < len(outdata):
             outdata[:len(data)] = data
             outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
+            loop.call_soon_threadsafe(event.set)
             raise sd.CallbackStop
         else:
             outdata[:] = data
 
-    event = threading.Event()
     stream = sd.RawOutputStream(
         samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE,
-        device="default", channels=CHANNELS, dtype='float32',
+        device=DEVICE, channels=CHANNELS, dtype='float32',
         callback=callback, finished_callback=event.set)
+        
     with stream:
-        event.wait()  # Wait until playback is finished
+        await event.wait()  # Wait until playback is finished
 
-record_buffer()
-play_buffer()
+async def main():
+    await record_buffer()
+    await play_buffer()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        sys.exit('\nInterrupted by user')
 
 # async def hello(uri):
     # async with connect(uri) as websocket:
